326fc4e3b61a39549981c44a02641f150efa8b94
Alessandro Franceschi <al@example42.com>
2016-05-03 15:32:11 +0200

    Added base docker class

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,18 +2,52 @@
 #
 class docker (
 
-  String[1]               $ensure              = 'present',
+  Variant[Boolean,String] $ensure           = present,
+
+  Hash                    $confs            = { },
+  Hash                    $dirs             = { },
+
+  Hash                    $options          = { },
+  Hash                    $settings         = { },
+
+  Array                   $profiles         = [],
+
+  Boolean                 $auto_restart     = true,
+  Boolean                 $auto_conf        = false,
 
   Variant[Undef,Hash]     $run                 = undef,
   Variant[Undef,Hash]     $build               = undef,
   Variant[Undef,Hash]     $test                = undef,
   Variant[Undef,Hash]     $push                = undef,
 
-  Hash                    $settings_hash       = {},
-
-  String[1]               $data_module         = 'tinydata',
+  String[1]               $data_module         = 'docker',
+  String[1]               $tinydata_module     = 'tinydata',
 
   ) {
+
+
+  $tp_settings = tp_lookup('docker','settings',$data_module,'merge')
+  $module_settings = $tp_settings + $settings
+  if $module_settings['service_name'] and $auto_restart {
+    $service_notify = "Service[${module_settings['service_name']}]"
+  } else {
+    $service_notify = undef
+  }
+
+  tp::install { 'docker':
+    options_hash  => $options,
+    settings_hash => $module_settings,
+    data_module   => $data_module,
+    conf_hash     => $confs,
+    dir_hash      => $dirs,
+    auto_conf     => $auto_conf,
+  }
+
+  if $profiles != [] {
+    $profiles.each |$kl| {
+      include "::docker::profile::${kl}"
+    }
+  }
 
   if $run {
     create_resources('docker::run', $run )
